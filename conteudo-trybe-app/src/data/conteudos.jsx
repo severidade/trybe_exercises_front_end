2145490558366b1a03e3f816ede5415a4108f3f2
severo <oliveira.severo@gmail.com>
2022-08-15 19:22:18 -0300

    feat: :rocket: Criando um arquivo separado do componente para inserir os dados

---
+++ conteudo-trybe-app/src/data/conteudos.jsx
@@ -0,0 +1,28 @@
+const conteudos = [
+  {
+    id: '1',
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    id: '2',
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    id: '3',
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    id: '4',
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+]
+
+export default conteudos;
