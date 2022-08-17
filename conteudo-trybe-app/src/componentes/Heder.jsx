35387bd8e3d55375c9fdd863fd08d9ca553a1bf8
severo <oliveira.severo@gmail.com>
2022-08-17 11:36:05 -0300

    feat: :art: formatacao do nav e destaque aos itens quando selecionados

--- conteudo-trybe-app/src/componentes/Heder.jsx
+++ conteudo-trybe-app/src/componentes/Heder.jsx
@@ -4,10 +4,10 @@ import Menu from './Menu';
 class Heder extends React.Component {
   render() {
     return (
-      <>
+      <div className='menuSuperior'>
         <h1>Conteúdos de Front-End</h1>
         <Menu />
-      </>
+      </div>
     );
   }
 }
