00a0a061bbea0468cf2d3a4154d426848aad5e8a
severo <oliveira.severo@gmail.com>
2022-08-19 10:38:31 -0300

    style: :art: Aprimoramentos no css

--- 10_2-Componentes_react/10_02_exercicios_fixacao/src/App.js
+++ 10_2-Componentes_react/10_02_exercicios_fixacao/src/App.js
@@ -7,14 +7,14 @@ import './App.css'
 class App extends React.Component {
 
   render() {
-    const url = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg";
+    const url = "https://severidade.com.br/bhl/img/blog/floralretangular.jpg";
   
     return (
-      <>
-        <h1>Gatinho</h1>
+      <div className='conteiner'>
+        <h1>Imagens como props</h1>
         <Image font="Arquivo de imagem importado do diretório" source={cat} alternativeText="Cute cat staring"/>
         <Image font="Imagem através de uma url" source={url} alternativeText="Cute cat staring"/>
-      </>
+      </div>
     );
   }
 }
--- 10_2-Componentes_react/10_02_exercicios_fixacao/src/Image.js
+++ 10_2-Componentes_react/10_02_exercicios_fixacao/src/Image.js
@@ -3,12 +3,14 @@ import React from 'react';
 class Image extends React.Component {
   render() {
     return (
-      <>
-        <h2>{this.props.font}</h2>
+      <div className='container_exemplo'>
+        <div className='container_titulo'>
+          <h2>{this.props.font}</h2>
+        </div>
         <div className='conteiner_image'>
           <img src={this.props.source} alt={this.props.alternativeText} />
         </div>
-      </>
+      </div>
     );
   }
 }
