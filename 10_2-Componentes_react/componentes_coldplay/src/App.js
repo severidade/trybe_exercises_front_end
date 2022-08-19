e1df27b9dd2746b43d0d236f08383bdbf1596fde
severo <oliveira.severo@gmail.com>
2022-08-19 12:11:32 -0300

    feat: :rocket: Criando os componetes para album

--- 10_2-Componentes_react/componentes_coldplay/src/App.js
+++ 10_2-Componentes_react/componentes_coldplay/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import Album from './componentes/Album';
 
 class App extends React.Component {
   render() {
@@ -36,20 +36,8 @@ class App extends React.Component {
     // Retorno do que será renderizado
     return (
       <article>
-        <section>
-          <img src={ album01.image } alt={ album01.title } />
-          <h2>{ album01.title }</h2>
-          <p>Lançamento: { album01.releaseDate.year }</p>
-          <p>Gravadora: { album01.others.recordCompany }</p>
-          <p>Formatos: { album01.others.formats }</p>
-        </section>
-        <section>
-          <img src={ album02.image } alt={ album02.title } />
-          <h2>{ album02.title }</h2>
-          <p>Lançamento: { album02.releaseDate.year }</p>
-          <p>Gravadora: { album02.others.recordCompany }</p>
-          <p>Formatos: { album02.others.formats }</p>
-        </section>
+        <Album album={ album01 } />
+        <Album album={ album02 } />
       </article>
     );
   }
